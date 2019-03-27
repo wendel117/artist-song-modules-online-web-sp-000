19549112dc86faa1751bd296d79a8a7d13f80f4f
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-27 14:23:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,14 +20,6 @@ extend Memorable::ClassMethods
     @@songs
   end
 
-#  def self.reset_all
-#    self.all.clear
-#  end
-
-#  def self.count
-#    self.all.count
-#  end
-
   def artist=(artist)
     @artist = artist
   end
