e46717e95c3e51acd6c02735a7854a8e2d67b2dd
Wendy Hamilton <wdhamilton117@gmail.com>
2019-03-27 14:37:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,9 +11,9 @@ include Paramable
 
   @@songs = []
 
-  def initialize
-    self.class.all << self
-  end
+#  def initialize
+#    self.class.all << self
+#  end
 
 #  def self.find_by_name(name)
 #    @@songs.detect{|a| a.name == name}
